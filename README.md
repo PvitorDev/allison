f00dab92c2fcb872075f24725e97cda51075553c
Paulo Vitor <pvitor.dev@gmail.com>
2022-05-18 18:50:53 -0300

    21 ANOS

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# SITE CRIADO PARA O MEU AMIGO ALLISON
+
+
+Apenas um simples presente de aniversário kkkk
+
---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+console.log(
+`                                                  
+                                                  
+PPPPPPPPPPPPPPPPP                                 
+P::::::::::::::::P                                
+P::::::PPPPPP:::::P                               
+PP:::::P     P:::::P                              
+  P::::P     P:::::Pvvvvvvv           vvvvvvv     
+  P::::P     P:::::P v:::::v         v:::::v      
+  P::::PPPPPP:::::P   v:::::v       v:::::v       
+  P:::::::::::::PP     v:::::v     v:::::v        
+  P::::PPPPPPPPP        v:::::v   v:::::v         
+  P::::P                 v:::::v v:::::v          
+  P::::P                  v:::::v:::::v           
+  P::::P                   v:::::::::v            
+PP::::::PP                  v:::::::v             
+P::::::::P                   v:::::v              
+P::::::::P                    v:::v               
+PPPPPPPPPP                     vvv       
+
+oooooooooo       ooooo  oooo      ooooo            ooooooooooo       oooooooo8  
+ 888    888       888    88        888              888    88       888         
+ 888oooo88        888    88        888              888ooo8          888oooooo  
+ 888  88o         888    88        888      o       888    oo               888 
+o888o  88o8        888oo88        o888ooooo88      o888ooo8888      o88oooo888  
+                                                                                 
+
+Perdeu alguma coisa? Otario!       
+                                            `)
+
+
+                                            
+
+
+
